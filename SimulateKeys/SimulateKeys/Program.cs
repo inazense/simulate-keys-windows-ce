592bf087e7a8103b51ef19728ab279f74b8d6112
Inazio Claver <jclaver64@gmail.com>
2017-05-08 15:06:33 +0200

    Add files via upload

---
+++ SimulateKeys/SimulateKeys/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Terranova.API;
+
+namespace SimulateKeys
+{
+    class Program
+    {
+        // Properties
+        const int KEYEVENT_KEYUP = 0x2; // Press code
+        const int KEYEVENT_KEYDOWN = 0x0; // Release code
+
+        // Methods
+
+        /// <summary>
+        /// Set the process you want to send the key as foreground window
+        /// </summary>
+        [DllImport("coredll.dll", EntryPoint = "SetForegroundWindow")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        /// <summary>
+        /// Send the key using .net compact framework 
+        /// </summary>
+        [DllImport("coredll.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        /// <summary>
+        /// Get the PID of the process passed by name
+        /// </summary>
+        /// <param name="processName">Process name</param>
+        /// <returns>PID of the process with the same name as the parameter</returns>
+        private static IntPtr getMyProcessPID(string processName)
+        {
+            IntPtr pid = new IntPtr();
+            // List all the running processes in the device
+            ProcessInfo[] processesList = ProcessCE.GetProcesses();
+
+            // Loop all the processes searching the name of the proccess we want to send keys
+            foreach (ProcessInfo process in processesList)
+            {
+                if (process.FullPath.Equals(processName))
+                {
+                    pid = process.Pid;
+                    break;
+                }
+            }
+
+            return pid;
+        }
+
+        // Main
+        static void Main(string[] args)
+        {
+            // What key I will to send in hexadecimal. All the possible key are in Keys.cs
+            byte key = Keys.ENTER;
+
+            // PID of the process to which I will send the key
+            IntPtr myProcessPID = getMyProcessPID("MyProcess.exe");
+            
+            if (myProcessPID != null)
+            {
+                // Get the process with the same PID as myProcessPID
+                Process myProcess = Process.GetProcessById(myProcessPID.ToInt32());
+
+                if (myProcess != null)
+                {
+                    // Set myProcess as foreground
+                    IntPtr foregroundProcess = myProcess.MainWindowHandle;
+                    SetForegroundWindow(foregroundProcess);
+
+                    // Send the key
+                    keybd_event(key, 0, KEYEVENT_KEYDOWN, 0); // Press the key
+                    keybd_event(key, 0, KEYEVENT_KEYUP, 0); // Release the key
+                }
+            }
+        }
+    }
+}
---
+++ SimulateKeys/SimulateKeys/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("SimulateKeys")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SimulateKeys")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("921a0d2b-0223-4a59-9ab8-62467db0c245")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de versión de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+
